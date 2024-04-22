c24796dff14b2e7992e583f83116180ddcae8a68
ikerlobop <ikerlobop@gmail.com>
2024-04-22 20:05:48 +0200

    Property Binding

--- ejemplos/src/app/app.component.ts
+++ ejemplos/src/app/app.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CabeceraComponent } from './cabecera/cabecera.component';
-import { CuerpoComponent } from './cuerpo/cuerpo.component';
-import { PieComponent } from './pie/pie.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabeceraComponent, PieComponent, CuerpoComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    HomeComponent,
+  ],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ejemplos';
+  title = 'homes';
 }
--- ejemplos/src/app/housing-location/housing-location.component.ts
+++ ejemplos/src/app/housing-location/housing-location.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation';
-
 @Component({
   selector: 'app-housing-location',
   standalone: true,
